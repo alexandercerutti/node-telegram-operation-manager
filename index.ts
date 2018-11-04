9741d98f880572569e9532026e4be904e2fd0e99
Alexander Cerutti <cerutti.alexander@gmail.com>
2018-11-04 21:02:15 +0100

    Various edits

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export { default as ReplyManager } from "./src/reply_manager";
 export { default as OperationManager } from "./src/operation_manager";
 
-// Should be imported only if you want to create your new queue
+// QueueManager should be imported only if you want to create your new queue
 // with blackjack and hookers (cit.)
 export { default as QueueManager } from "./src/queue_manager";
